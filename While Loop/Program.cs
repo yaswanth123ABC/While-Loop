a134aa57c186aa62ee014ed364a838ab6e051a00
yaswanth <yaswanth@turingsystems.com>
2022-02-09 15:09:58 +0530

    Add project files.

---
+++ While Loop/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Conditional
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1;
+            while (i <= 4)
+            {
+                Console.WriteLine("i value: {0}", i);
+                i++;
+            }
+            Console.WriteLine("Press Enter Key to Exit..");
+            Console.ReadLine();
+        }
+    }
+}
